9ff36f9cbda5f1a1cabd378179b8569c627bf6e3
millernerd <millernerd@Framework.localdomain>
2021-11-18 10:55:29 -0700

    sendFile index.html

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 app.get('/', (req, res) => {
-    res.send("Hello Express")
+    // res.send("Hello Express")
+    res.sendFile(__dirname + '/views/index.html')
   })
 
  module.exports = app;
